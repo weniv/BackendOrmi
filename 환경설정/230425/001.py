ad4f52699e3f5bb69a24a116d510caf23de311a8
hojun.lee <60565211+paullabkorea@users.noreply.github.com>
2023-04-25 15:07:38 +0900

    Add files via upload

---
+++ 환경설정/230425/001.py
@@ -0,0 +1,13 @@
+# 스니펫 설정
+# {
+# 	"Print to console": {
+# 		"prefix": "def",
+# 		"body": [
+# 			"def solution($1):",
+# 			"    return $2"
+# 		],
+# 		"description": "function 제작"
+# 	}
+# }
+def hello(world):
+    return world
