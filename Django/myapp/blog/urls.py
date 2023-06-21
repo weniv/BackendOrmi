7686a5adb3d18b7e1f907a0e9b574155eac6736c
Harry <eusun0830@gmail.com>
2023-06-21 11:52:11 +0900

    Django day2 오전

--- Django/myapp/blog/urls.py
+++ Django/myapp/blog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     # path(패턴, 매핑) /blog/
     # path("", views.index), # FBV
-    path("", views.Index.as_view())
-    # 글 조회
+    # 글 목록 조회
+    path("", views.Index.as_view(), name='list'),
     # 글 작성
+    path("write/", views.write, name='write'),
     # 글 수정
     # 글 삭제
     # 코멘트 작성
--- Django/myapp/blog/views.py
+++ Django/myapp/blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 # def index(request):
@@ -16,5 +18,27 @@ class Index(View):
         # return HttpResponse('Index page GET class')
         
         # 데이터베이스에 접근해서 값을 가져와야 합니다.
+        # 게시판에 글들을 보여줘야하기 때문에 데이터베이스에서 "값 조회"
+        # MyModel.objects.all()
+        post_objs = Post.objects.all()
         # context = 데이터베이스에서 가져온 값
-        return render(request, 'blog/board.html')
+        context = {
+            "posts": post_objs
+        }
+        # print(post_objs)
+        return render(request, 'blog/board.html', context)
+
+
+# write
+# post - form
+# 글 작성 화면
+def write(request):
+    if request.method == 'POST':
+        # form 확인
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('blog:list')
+
+    form = PostForm()
+    return render(request, 'blog/write.html', { 'form': form })
